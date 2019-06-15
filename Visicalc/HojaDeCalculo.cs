6d47f13698714fb966314a6bbb0370d855563f8c
Militchick <39128819+Militchick@users.noreply.github.com>
2019-06-15 17:23:30 +0200

    v0.04

--- Visicalc/HojaDeCalculo.cs
+++ Visicalc/HojaDeCalculo.cs
@@ -262,7 +262,6 @@ namespace Visicalc
 
                         if(guardarsino == false)
                         {
-
                             bool bucle = true;
 
                             do
